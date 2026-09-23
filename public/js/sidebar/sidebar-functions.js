0462cc6dd528bd8917ad22743ade233ff7c970e1
Genrev <genrevvaguilar@gmail.com>
2026-09-23 15:24:09 +0800

    fix: solve loadPage() pagePath variable problem

--- public/js/sidebar/sidebar-functions.js
+++ public/js/sidebar/sidebar-functions.js
@@ -7,21 +7,21 @@ const user = getCurrentUser();
 
 export async function loadPage(page) {
     // js for testing, remove the products page specific constraint later
-    //const pagePath = page === 'products' ? `/views/pages/${user.user_type}/${page}.html` : '/views/dummy.html';
+    // const pagePath = page === 'products' ? `/views/pages/${user.user_type}/${page}.html` : '/views/dummy.html';
 
-    //set to dashboard for front end development
-    const pagePath = "/views/pages/admin/dashboard.html"
+    const pagePath = `/views/pages/${user.user_type}/${page}.html`;
+    console.log(pagePath)
 
     const response = await fetch(pagePath);
 
-    // if (!response.ok) {
-    //     mainContent.innerHTML = `
-    //         <div class="container p-2">
-    //             Can't load content hehe :)
-    //         </div>
-    //     `;   
-    //     return;
-    // }
+    if (!response.ok) {
+        mainContent.innerHTML = `
+            <div class="container p-2">
+                Can't load content hehe :)
+            </div>
+        `;   
+        return;
+    }
 
     mainContent.innerHTML = await response.text();
 
